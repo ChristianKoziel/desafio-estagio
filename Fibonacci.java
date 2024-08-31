d87c151751d7bc5809ed7b0eadb3ff1ac934198f
Christian Koziel <christiankoziel@hotmail.com>
2024-08-31 18:38:46 -0300

    Desafio numero 2 Fibonacci

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,13 +1,32 @@
-public class Soma {
+import javax.swing.JOptionPane;
 
+public class Fibonacci {
     public static void main(String[] args) {
-        int indice = 13;
-        int soma = 0;
-        int k = 0;
-        for (k = 1; k <= indice; k++) {
-            soma = soma + k;
-        }
+        String input = JOptionPane.showInputDialog("Informe um número para verificar se pertence à sequência de Fibonacci:");
+        int numero = Integer.parseInt(input); 
 
-        System.out.println("O valor da soma é: "+soma);
+        int a = 0, b = 1;
+        boolean pertence = false;
+
+        if (numero == a || numero == b) {
+            pertence = true; 
+        } else {
+            while (b <= numero) {
+                int proximo = a + b;
+                a = b; 
+                b = proximo; 
+                int teste=1;
+
+                if (b == numero) {
+                    pertence = true; 
+                    break; 
+                }
+            }
+        }
+        if (pertence) {
+            JOptionPane.showMessageDialog(null, "O número " + numero + " pertence à sequência de Fibonacci.");
+        } else {
+            JOptionPane.showMessageDialog(null, "O número " + numero + " NÃO pertence à sequência de Fibonacci.");
+        }
     }
 }
